a34e666ae9502bba56c25bad218e7f303aa71f6f
betiol <nikollasbetiol@gmail.com>
2017-09-08 11:49:10 -0400

    add new endpoint

--- app/services/Sinesp.js
+++ app/services/Sinesp.js
@@ -1,30 +1,36 @@
 // @flow
-const sinesp = require('sinesp-nodejs');
 
-export default async function querySinesp(placa: string) {
+export default async function querySinesp(placa) {
     if (!placa) {
         return {
-            error: 'Invalid City',
+            error: 'Placa Inválida',
         };
     }
 
-    const response = await sinesp.consultaPlaca(`${placa}`, (car) => {
+    const response = await fetch(`http://betiol-github-io.umbler.net/api/consultaPlaca?placa=${placa}`);
+
+    if (!response.ok) {
         return {
-            codigoRetorno: car.codigoRetorno,
-            mensagemRetorno: car.mensagemRetorno,
-            codigoSituacao: car.codigoSituacao,
-            situacao: car.situacao,
-            modelo: car.modelo,
-            marca: car.marca,
-            cor: car.cor,
-            ano: car.ano,
-            anoModelo: car.anoModelo,
-            placa: car.placa,
-            data: car.data,
-            uf: car.uf,
-            municipio: car.municipio,
-            chassi: car.chassi
-        }
-        return response;
-    })
+            error: 'Erro ao obter dados dessa placa',
+        };
+    }
+
+    const car = await response.json().then(res => res);
+    return {
+        codigoRetorno: car.codigoRetorno,
+        mensagemRetorno: car.mensagemRetorno,
+        codigoSituacao: car.codigoSituacao,
+        situacao: car.situacao,
+        modelo: car.modelo,
+        marca: car.marca,
+        cor: car.cor,
+        ano: car.ano,
+        anoModelo: car.anoModelo,
+        placa: car.placa,
+        data: car.data,
+        uf: car.uf,
+        municipio: car.municipio,
+        chassi: car.chassi
+    };
+
 }
